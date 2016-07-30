dcaa62c69f7c2977e37db9241d04fe7a7094cede
XLuoChen <1425145741@qq.com>
2016-07-31 01:13:04 +0800

    add components

---
+++ components/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const RenderEvents = React.createClass({
+  render: function () {
+    return (<p>haha</p>)
+  }
+});
+ReactDOM.render(<RenderEvents />, document.getElementById('test'));
